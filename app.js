743d4dcd62bc93b1b9aeac5dd78a2938753655d0
ReeceRenninger <renningerreece@gmail.com>
2023-01-12 13:18:58 -0800

    js notes updated

--- app.js
+++ app.js
@@ -2,19 +2,20 @@
 // Need to check the user input as part of lab 7
 //had to set userName to a variable for it to be called OUTSIDE of original function. must use return statment to store userName in starting function 
 // user prompt on webpage open
-
+// Could call the function in itself to reprompt over and over until null or false is no longer present, but cant return information
 function userInput() {
   let userName = prompt("If you love Pizza, tell me your name traveler?");
 
   if (userName == false) {
-    userName = prompt("I must know your name for your journey to continue.");
+    userName = prompt("You wouldn't want to be known as stranger, right?");
 
   } if (userName == false){
     document.write("Stranger Danger, refresh and try again!")
-  }
-  else {
+
+  }else {
     document.write("Welcome to ZAAAAALAANNNDDD " + userName.toUpperCase() + "!");
   }
+
   alert("You put your name right?")
   alert("If not... you're going to be a stranger.....")
   return userName;
