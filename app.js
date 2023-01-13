687a0f077cadabd499785644c9dae847e4c050bb
ReeceRenninger <renningerreece@gmail.com>
2023-01-12 16:23:58 -0800

    please work on live now

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ function userInput() {
 
 let userAmount = prompt ("How many slices do you want?");
 function repeatImage(){
-
 for (let i=0; i < userAmount; i++){
 document.write('<img src="https://images.unsplash.com/photo-1601924582970-9238bcb495d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80">')}
 }
